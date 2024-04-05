45cffd473d15a2cf1caf6c2460f0a44a9b94e479
Kévin DONIN DE ROSIERE <kevindoninderosiere@gmail.com>
2024-04-05 13:40:55 +0200

    Attempting to fix unexpected disconnect while reading sideband packet

--- entrypoint.sh
+++ entrypoint.sh
@@ -120,4 +120,5 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
+git config http.postBuffer 524288000
 git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
